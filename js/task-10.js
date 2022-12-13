f7128e4f065ec61aa076434111e079cdf9b6904d
Magdalena Grądek <magdalena.gradek@gmail.com>
2022-12-13 14:19:49 +0100

    zad 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divBoxes = document.getElementById("boxes");
+const createBoxes = document.querySelector("[data-create]");
+const destroyBoxes = document.querySelector("[data-destroy]");
+const amount = document.querySelector("input");
+
+const startSize = 30;
+let boxSize = 30;
+
+createBoxes.addEventListener("click", () =>{
+ for (let i = 0; i < amount.value; i++) {
+const newBox = document.createElement("div");
+
+newBox.style.width = `${boxSize}px`;
+newBox.style.height = `${boxSize}px`;
+newBox.style.backgroundColor = `${getRandomHexColor()}`;
+   boxSize += 10;
+   divBoxes.appendChild(newBox);
+ }
+});
+
+
+destroyBoxes.addEventListener("click", () =>{
+  divBoxes.innerHTML = "";
+  boxSize = startSize;
+});
