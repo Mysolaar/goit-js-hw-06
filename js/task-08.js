316a4748e7b76a7bc45eac9ba4be75bcfc7d956d
Magdalena Grądek <magdalena.gradek@gmail.com>
2022-12-13 12:13:43 +0100

    zad 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,13 +1,37 @@
+// const loginForm = document.querySelector(".login-form");
+
+// loginForm.addEventListener("submit", submitForm);
+
+// function submitForm(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password }
+//   } = event.currentTarget; 
+
+//   if (email.value === "" || password.value === "") {
+//     window.alert("Please fill in all the fields!");
+//   } else {
+//     const formData = {
+//       email: email.value,
+//       password: password.value
+//     };
+
+//     console.log(formData);
+//     event.currentTarget.reset();
+//   }
+// }
+
+/// wrzucone w funkcję 
+
 const loginForm = document.querySelector(".login-form");
 
-loginForm.addEventListener("submit", submitForm);
-
-function submitForm(event) {
-  event.preventDefault();
-  const {
+loginForm.addEventListener("submit", (event) => {
+ event.preventDefault();
+const {
     elements: { email, password }
   } = event.currentTarget; 
 
+  
   if (email.value === "" || password.value === "") {
     window.alert("Please fill in all the fields!");
   } else {
@@ -15,8 +39,7 @@ function submitForm(event) {
       email: email.value,
       password: password.value
     };
-
-    console.log(formData);
+ console.log(formData);
     event.currentTarget.reset();
   }
-}
+});
